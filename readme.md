44b923eaf4c6fe593446ba39d20387e825913d80
djf@evm <djf@evm.com>
2020-10-10 15:49:30 +0800

    添加项目

---
+++ readme.md
@@ -0,0 +1,50 @@
+一.项目简介
+========================================
+
+
+本项目是[**`EVM超轻量物联网虚拟机`**](https://gitee.com/scriptiot/evm)团队深入研究鸿蒙智能手表的官方demo后的测试用例，主要用于基于模拟器`体验和学习鸿蒙的hml`开发方式；
+
+
+
+二.打开项目
+========================================
+
+
++ 打开 `DevEcoStduio`
+
++ 点击 `File/Open`
+
+    ![open_projects](images/open_projects.png)
+
++ 点击 `build`
+
+    ![run](images/run.png)
+
++ 选择模拟器 `Huawei Lite Wearable Simulator` 点击OK
+
+    ![simulator](images/simulator.png)
+
++ 自动弹出模拟器查看界面效果
+
+    ![showcase_index](images/showcase_index.png)
+
+
+三、测试展示
+========================================
+
+
+
+四、加入EVM官方QQ技术交流群
+========================================
+
+> 群号：**`399011436`**
+
+![evm](images/QQ.jpg)
+
+五、联系我们
+========================================
++ 邮箱： scriptiot@aliyun.com
++ 微信：  
+    ![evm](images/djf.png)
+
+欢迎大家通过 `GitHub issues`、`邮件` `微信` 或 `QQ群`反馈问题和联系我们。
--- showcase.py
+++ showcase.py
@@ -1,3 +1,121 @@
 #!/usr/bin/env python
 # -*- coding: utf_8 -*-
 
+import os
+import shutil
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+logPath = os.getcwd() + os.path.sep + "logs"
+if not os.path.exists(logPath):
+    os.makedirs(logPath)
+
+fh = RotatingFileHandler("showcase.log", maxBytes=10 * 1024 * 1024, backupCount=100)
+fh.setLevel(logging.DEBUG)
+#log write in console
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+#log formatter
+formatter = logging.Formatter(
+    '%(asctime)s %(levelname)8s [%(filename)25s%(funcName)20s%(lineno)06s] %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+logger = logging.root
+logger.setLevel(logging.INFO)
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+
+def copydirs(from_file, to_file):
+    if not os.path.exists(to_file):  # 如不存在目标目录则创建
+        os.makedirs(to_file)
+    files = os.listdir(from_file)  # 获取文件夹中文件和目录列表
+    for f in files:
+        if os.path.isdir(from_file + os.sep + f):  # 判断是否是文件夹
+            copydirs(from_file + os.sep + f, to_file + os.sep + f)  # 递归调用本函数
+        else:
+            shutil.copy(from_file + os.sep + f, to_file + os.sep + f)  # 拷贝文件
+
+def rmdirs(dirpath):
+    if os.path.exists(dirpath):
+        files = os.listdir(dirpath)
+        for file in files:
+            filepath = os.path.join(dirpath, file).replace("\\",'/')
+            if os.path.isdir(filepath):
+                rmdirs(filepath)
+            else:
+                os.remove(filepath)
+        os.rmdir(dirpath)
+
+def generateProjects(path):
+    '''
+        获取指定path下的所有文件列表
+    '''
+    ignore_dir = ['.git']
+    templateDir = os.sep.join([os.getcwd(), "helloworld"])
+    files = []
+    for dirpath, dirnames, filenames in os.walk(path):
+        for d in ignore_dir:
+            # print(dirpath)
+            # print(os.sep.join([os.getcwd(), d]))
+            # print(dirpath.startswith(os.sep.join([os.getcwd(), d])))
+            if dirpath.startswith(os.sep.join([os.getcwd(), d])):
+                continue
+        # print(dirpath, dirnames, filenames)
+        if filenames == ['index.css', 'index.hml', 'index.js']:
+            # componentID = "%s-%s" %    os.path.basename(dirpath)
+            pathkeys = dirpath.split(os.sep)
+            if 'component' in dirpath:
+                index = pathkeys.index('component')
+                projectName = "showcase-%s" % "-".join(pathkeys[index+1:])
+            else:
+                index = pathkeys.index('examples')
+                projectName = pathkeys[index+1]
+                if projectName == "showcase":
+                    projectName = "showcase-index"
+            
+            #创建目录
+            projectDir = os.sep.join([os.getcwd(), projectName])
+            if os.path.exists(projectDir):
+                rmdirs(projectDir)
+            if not os.path.isdir(projectDir):
+                os.makedirs(projectDir)
+
+            logger.info("generate project [%s]-[%s]" % (projectName, projectDir))
+            copydirs(templateDir , projectDir)
+
+            defaultDir = os.sep.join(pathkeys[0:pathkeys.index('default')+1])
+            projectDir =  os.sep.join([projectDir, "entry", "src", "main", "js","default"])
+            if os.path.exists(defaultDir):
+                copydirs(defaultDir , projectDir)
+                rmdirs(os.sep.join([projectDir, "pages"]))
+            
+            projectPageDir = os.sep.join([projectDir, "pages"])
+            if os.path.exists(projectPageDir):
+                rmdirs(projectPageDir)
+            os.makedirs(projectPageDir)
+
+            projectIndexDir = os.sep.join([projectPageDir, "index"])
+            if os.path.exists(projectIndexDir):
+                rmdirs(projectIndexDir)
+            # 拷贝源文件index.hml index.css index.js
+            # indexDir = os.sep.join([projectDir, "entry", "src", "main", "js","default","pages","index"])
+            os.makedirs(projectIndexDir)
+            copydirs(dirpath , projectIndexDir)
+
+            indexFile = os.sep.join([projectIndexDir, "index.js"])
+            with open(indexFile, "r") as f:
+                content = str(f.read())
+            if "../../../../common/js/general" in content:
+                content = content.replace("../../../../common/js/general", "../../common/js/general")
+                with open(indexFile, "w") as f:
+                    f.write(content)
+
+def main():
+    generateProjects(os.sep.join([os.getcwd(), "examples"]))
+
+
+if __name__ == '__main__':
+    main()
